556b1431886dc88351c8257b1574f1b3a4c05f9d
Volodymyr <vovatsyukh@gmail.com>
2025-02-02 17:38:46 +0200

    upd

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["eslint.config.mjs"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -17,19 +17,43 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        project: "./tsconfig.json", // Потрібно для правильного аналізу коду
+        tsconfigRootDir: process.cwd(),
       },
     },
   },
   {
+    plugins: ["@typescript-eslint", "prettier"],
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "plugin:prettier/recommended",
+    ],
+  },
+  {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "no-console": "warn",
+      "prettier/prettier": "error",
     },
   },
+  {
+    overrides: [
+      {
+        files: ["*.ts"],
+        parser: "@typescript-eslint/parser",
+        parserOptions: {
+          project: "./tsconfig.json",
+        },
+      },
+    ],
+  }
 );
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ClubModule } from './club/club.module';
+import { BookModule } from './book/book.module';
+import { LibraryModule } from './library/library.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ClubModule, BookModule, LibraryModule],
   controllers: [],
   providers: [],
 })
